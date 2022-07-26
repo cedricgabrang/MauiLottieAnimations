5dda45cb2e4c200c07f240f66449ddb20dd4c8f9
Cedric Gabrang <cedric.gabrang@hotmail.com>
2022-07-26 20:29:33 +0800

    Add project files.

---
+++ MauiLottieAnimations/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiLottieAnimations;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MauiLottieAnimations/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiLottieAnimations;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
